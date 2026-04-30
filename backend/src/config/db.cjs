32c666153d41ba06dffe849006e6b253bb8dbee3
Luis Manuel <luismanuel@SGBR.intra>
2026-04-30 17:34:57 -0300

    Banco novo vendaflow

--- backend/src/config/db.cjs
+++ backend/src/config/db.cjs
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: process.env.DB_USER || 'postgres',
   host: process.env.DB_HOST || 'localhost',
-  database: process.env.DB_NAME || 'painel_comercial',
+  database: process.env.DB_NAME || 'vendaflow',
   password: process.env.DB_PASSWORD || '1234',
   port: Number(process.env.DB_PORT) || 5432,
 });
